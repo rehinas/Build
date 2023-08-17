6abc8d5eb199e010f846084ee5ca364667baeab1
rehinas1 <124281503+rehinas1@users.noreply.github.com>
2023-08-18 01:01:30 +0530

    COMMIT

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+
+const path = require('path'); 
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+const PORT = process.env.PORT;
+
+app.use(express.static(path.join(__dirname,'/build')))
+const apiRouter = require('./Router/empr');
+const userRouter = require('./Router/Usere');
+
+app.use('/api', apiRouter);
+app.use('/api', userRouter);
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+require('./db');
+app.get('/*',function(req,res){
+  res.sendFile(path.join(__dirname,'/buil/index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
